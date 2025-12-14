3ac108f389ab997873f34a771c4847ab00272755
taito-station <hanshin.abingdon@gmail.com>
2025-12-14 13:01:31 +0900

    add logger

--- sources/apps/rest-server/src/bin.rs
+++ sources/apps/rest-server/src/bin.rs
@@ -1,15 +1,19 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::env;
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 async fn greet(req: HttpRequest) -> impl Responder {
