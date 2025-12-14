e5aa09abf216d5e3bea468e3f0f9d84b360e3db6
taito-station <hanshin.abingdon@gmail.com>
2025-12-14 13:31:18 +0900

    add log config

--- sources/libs/config/src/lib.rs
+++ sources/libs/config/src/lib.rs
@@ -1,13 +1,19 @@
 use dotenvy::dotenv;
+use envy::prefixed;
 use serde::Deserialize;
 use std::marker::PhantomData;
-use envy::prefixed;
 
 #[derive(Debug)]
 pub struct Config {
+    pub log: LogConfig,
     pub server: ServerConfig,
     pub db: DbConfig,
     _hide_default_constructor: PhantomData<()>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LogConfig {
+    pub level: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -26,10 +32,12 @@ impl Config {
     pub fn from_env() -> Result<Self, envy::Error> {
         dotenv().expect(".env file not found");
 
+        let log = prefixed("LOG_").from_env::<ServerConfig>()?;
         let server = prefixed("SERVER_").from_env::<ServerConfig>()?;
         let db = prefixed("DB_").from_env::<DbConfig>()?;
 
         Ok(Self {
+            log,
             server,
             db,
             _hide_default_constructor: PhantomData,
